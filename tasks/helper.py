168122552e0d2586add8d0b5cccc14376a507866
Vardan2009 <70532109+Vardan2009@users.noreply.github.com>
2026-01-09 17:26:45 +0400

    Finished opencv intro

--- tasks/helper.py
+++ tasks/helper.py
@@ -4,49 +4,35 @@ import matplotlib.pyplot as plt
 # Use this file to store functions that you'll use in the rest of the tasks in this class!
 
 def load_image(image_path: str) -> np.ndarray:
-    """
-    Use OpenCV to load an image from a file.
-    """
-    pass
+    return cv2.imread(image_path)
 
 def save_image(image: np.ndarray, image_path: str) -> None:
-    """
-    Use OpenCV to save an image to a file.
-    """
+    cv2.imwrite(image_path, image)
     pass
 
-def display_image(image: np.ndarray) -> None:
-    """
-    Use matplotlib to display an image.
-    """
+def display_image(image: np.ndarray, greyscale=False) -> None:
+    if greyscale:
+        plt.imshow(image, cmap='grey')
+    else:
+        plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), cmap='grey')
+    plt.axis('off')
+    plt.show()
     pass
 
 def crop_image(image: np.ndarray, x: int, y: int, width: int, height: int) -> np.ndarray:
-    """
-    Crop an image to a given rectangle.
-    """
-    pass
+    return image[y:(y+height), x:(x+width)]
 
-def edit_brightness(image: np.ndarray, brightness: int) -> np.ndarray:
-    """
-    Edit the brightness of an image.
-    """
-    pass
+def edit_brightness(image: np.ndarray, brightness_delta: int) -> np.ndarray:
+    return np.clip(image.astype(np.float32) + brightness_delta, 0, 255).astype(np.uint8)
 
-def edit_contrast(image: np.ndarray, contrast: int) -> np.ndarray:
-    """
-    Edit the contrast of an image.
-    """
-    pass
+def edit_contrast(image: np.ndarray, contrast: float) -> np.ndarray:
+    contrasted = (image.copy().astype(np.float32) - 128) * contrast + 128
+    return np.clip(contrasted, 0, 255).astype(np.uint8)
 
 def make_greyscale(image: np.ndarray) -> np.ndarray:
-    """
-    Make an image greyscale.
-    """
-    pass
+    return np.dot(image[..., :], [0.114, 0.587, 0.299])
 
-def tint_image(image: np.ndarray, color: tuple) -> np.ndarray:
-    """
-    Tint an image with a given color. Hint: look at the make_redder function!
-    """
-    pass
+def tint_image(image: np.ndarray, color: tuple, tint_weight=0.3) -> np.ndarray:
+    image_tinted = image.copy().astype(np.float32)
+    image_tinted = (1 - tint_weight) * image_tinted + tint_weight * np.array(color)    
+    return np.clip(image_tinted, 0, 255).astype(np.uint8)
